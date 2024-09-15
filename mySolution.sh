b15e195fe699f42436d1156e437a4ac9462d3c89
milind-vi <milindsmarty8@gmail.com>
2024-09-15 18:19:19 +0530

    final commit after changing

---
+++ mySolution.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+cd src
+if [ -d "maliciousFiles" ]; then
+    rm -rf maliciousFiles
+fi
+mkdir secretDir
+touch secretDir/.secret
+ls -la secretDir
+chmod 600 secretDir/.secret
+ls -l secretDir/.secret
+/bin/bash generateSecret.sh
